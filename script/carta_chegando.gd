cdb7783bb4ed697c076285688d7284ff88ced7e5
SoaresDavidson <davi504r35@gmail.com>
2024-07-08 12:30:14 -0300

    codigo de numeros aleatorios funcionando,paralax ativo e sprites do lucas na pasta de assets

--- script/carta_chegando.gd
+++ script/carta_chegando.gd
@@ -1,8 +1,9 @@
 extends AnimatableBody2D
 @onready var game = $".."
+
 @onready var animationchegando = $Animationchegando
 @onready var carta = $"../carta"
-
+signal personalidade
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -10,7 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if game.tem_carta== false :
+	if game.tem_carta == false and game.carta_chegando == false:
 		animationchegando.play("chegando")
 
 
@@ -18,5 +19,5 @@ func _on_button_pressed():
 	animationchegando.play("RESET")
 	carta.visible = true
 	game.tem_carta = true
-	
+	emit_signal("personalidade")
 	
--- script/game.gd
+++ script/game.gd
@@ -1,11 +1,14 @@
 extends Node
 
-var diff = 0
-var day = 0
+@onready var remetente = $texto/remetente
+@onready var destinario = $texto/destinario
+
+
 var tem_carta = false
 var carta_chegando = false
 var carta_saindo = false
-
+var escolha = 0
+var randperson=0
 
 var person1=[0,1,2,3]
 
@@ -15,7 +18,9 @@ var person3=[8,9,10,11]
 
 var person4=[12,13,14,15]
 
-var person=[person1,person2,person3,person4]
+var person_remetente=[]
+
+var person_destinatario=[]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -24,3 +29,49 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_carta_chegando_personalidade():
+	
+	for lista in range(4):
+		escolha = randi() % 4
+		if escolha==0:
+			randperson = randi_range(0,3)
+			person_remetente.append(person1[randperson])
+		if escolha==1:
+			randperson = randi_range(0,3)
+			person_remetente.append(person2[randperson])
+		if escolha==2:
+			randperson = randi_range(0,3)
+			person_remetente.append(person3[randperson])
+		if escolha==3:
+			randperson = randi_range(0,3)
+			person_remetente.append(person4[randperson])
+		
+		
+
+	escolha = 0
+	randperson = 0
+	for lista in range(4):
+		escolha = randi_range(0, 3)
+		if escolha==0:
+			randperson = randi_range(0,3)
+			person_destinatario.append(person1[randperson])
+		if escolha==1:
+			randperson = randi_range(0,3)
+			person_destinatario.append(person2[randperson])
+		if escolha==2:
+			randperson = randi_range(0,3)
+			person_destinatario.append(person3[randperson])
+		if escolha==3:
+			randperson = randi_range(0,3)
+			person_destinatario.append(person4[randperson])
+		
+		
+	escolha=0
+	randperson=0
+	
+	remetente.text= "eu sou:\n"+str(person_remetente[0])+"\n"+str(person_remetente[1])+"\n"+str(person_remetente[2])+"\n"+str(person_remetente[3])+"\n"
+	destinario.text = "meu amor e:\n"+str(person_destinatario[0])+"\n"+str(person_destinatario[1])+"\n"+str(person_destinatario[2])+"\n"+str(person_destinatario[3])+"\n"
+
+
