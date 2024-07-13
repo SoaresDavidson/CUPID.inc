1a93c5613a44dda4135abeda1312a46a3b1a06a3
lucasvilari <vilarinholucas80@outlook.com>
2024-07-13 20:12:54 -0300

    Finais adicionados com condições estabelecidas no script main.

---
+++ script/incompetentefracasso.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _ready():
+	MenuMusic.get_child(17).play()
+	$Label.text = "Você não fez nada direito. Cometeu erros e não entregou a meta diária. Você manteve o emprego por " + str(GlobalVars.scoreatual) + " dias."
+	await get_tree().create_timer(1.0).timeout
+	$Button.show()
+
+
+func _on_button_pressed():
+	$Button.hide()
+	$AnimationPlayer.play("FadeOut")
+	MenuMusic.get_child(17).stop()
+	await get_tree().create_timer(1.0).timeout
+	GlobalVars.scoreatual = 0
+	get_tree().change_scene_to_file("res://scenes/menu.tscn")
